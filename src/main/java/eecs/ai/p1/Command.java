f047519d517e348cfb90313f846c9807cc5d0aaa
Kris Zhao <kriszhao@comcast.net>
2019-09-17 09:25:36 -0400

    Started structuring the files and outling process

---
+++ src/main/java/eecs/ai/p1/Command.java
@@ -0,0 +1,5 @@
+package eecs.ai.p1;
+
+public interface Command{
+    public void execute();
+}
---
+++ src/main/java/eecs/ai/p1/Commander.java
@@ -0,0 +1,18 @@
+package eecs.ai.p1;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Commander {
+    private final File filename;
+    private final List<Command> commands = new ArrayList<>();
+
+    private Commander (List<Command> commands){
+        this.filename = filename;
+    }
+
+    public final Commander of(File filename){
+        
+    }
+}
